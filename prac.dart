1e924e2d727657fb34ca4e6b49898a363e2a21fa
Tarannummm <tarannumelina@gmail.com>
2025-10-15 23:32:07 +0600

    swap

--- prac.dart
+++ prac.dart
@@ -24,3 +24,17 @@ void main() {
   int square = num * num;
   print("Square of $num is $square");
 }
+
+//swapping two numbers
+void main() {
+  int a = 5;
+  int b = 10;
+
+  print("Before swapping: a = $a, b = $b");
+
+  int temp = a;
+  a = b;
+  b = temp;
+
+  print("After swapping: a = $a, b = $b");
+}
