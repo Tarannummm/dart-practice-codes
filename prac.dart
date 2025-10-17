5592d1bc48039bb1476f8ed9c2bfca8b7b610737
Tarannummm <tarannumelina@gmail.com>
2025-10-17 23:40:15 +0600

    prac

--- prac.dart
+++ prac.dart
@@ -52,3 +52,27 @@ void main() {
   int area = length * width;
   print("Area of rectangle is $area");
 }
+
+
+//leapyear
+void main() {
+  int year = 2024;
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    print("$year is a leap year");
+  } else {
+    print("$year is not a leap year");
+  }
+}
+
+//factorial
+void main() {
+  int n = 5;
+  int fact = 1;
+
+  for (int i = 1; i <= n; i++) {
+    fact *= i;
+  }
+
+  print("Factorial of $n is $fact");
+}
